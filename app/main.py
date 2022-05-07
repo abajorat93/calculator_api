c2bb0604efca27c8efa4f366454272fbacdf3190
carlos.lopez <Carlos.Lopez@dowjones.com>
2022-05-07 08:13:23 -0500

    feat: add logger and loggings

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Path, Query
-from calculator import calculator as calc
+import logging
+from webbrowser import Opera
+from fastapi import FastAPI
+from math_ops.operations import Operations
+
 
 # DEBUG: Detailed information, typically of interest only when diagnosing problems.
 # INFO: Confirmation that things are working as expected.
@@ -7,33 +10,65 @@ from calculator import calculator as calc
 # ERROR: Due to a more serious problem, the software has not been able to perform some function.
 # CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
+formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
+
+file_handler = logging.FileHandler("logs_calculator.log")
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)  # Se agrega handler para stream
 app = FastAPI()
 
 
 @app.get("/sum/{v1}/{v2}")
 async def sum(v1: int, v2: int):
-    return {"resultado": int(calc.sum(v1, v2))}
+    ops = Operations()
+    result = int(ops.get_sum(v1, v2))
+    logger.debug(f"'resultado': {result}")
+    logger.info(f"'resultado': {result}")
+    return {"resultado": result}
 
 
-@app.get("/substract/{v1}/{v2}")
-async def substract(v1: float, v2: float):
-    return {"resultado": calc.subtract(v1, v2)}
+@app.get("/subtract/{v1}/{v2}")
+async def subtract(v1: float, v2: float):
+    ops = Operations()
+    result = int(ops.get_subtract(v1, v2))
+    logger.debug(f"'resultado': {result}")
+    logger.info(f"'resultado': {result}")
+    return {"resultado": result}
 
 
 @app.get("/divide/{v1}/{v2}")
 async def divide(v1: float, v2: float):
-    if v2 == 0:
+    ops = Operations()
+    if v2 == 0 or v1 == 0:
+        print("resultado: cannot divide with 0")
+        logger.error("'resultado': 'cannot divide with 0'")
         return {"resultado": "can't divide with 0"}
-    return {"resultado": calc.divide(v1, v2)}
+
+    result = int(ops.get_division(v1, v2))
+    logger.debug(f"'resultado': {result}")
+    logger.info(f"'resultado': {result}")
+    return {"resultado": result}
 
 
 @app.get("/multiply/{text}")
 async def multiply(text: str):
     try:
+        ops = Operations()
         values = text.split("*")
         v1 = values[0]
         v2 = values[1]
-        return {"resultado": calc.multiply(v1, v2)}
+        result = int(ops.get_multiplication(v1, v2))
+        logger.debug(f"'resultado': {result}")
+        logger.info(f"'resultado': {result}")
+        return {"resultado": result}
     except IndexError or ValueError:
+        logger.exception("Invalid multiplication string. Must be 'a*b'")
         return {"resultado": "invalid multiplication string. Must be 'a*b'"}
